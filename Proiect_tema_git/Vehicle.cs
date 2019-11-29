8dea8c73fa2f1abdd3a2f518ea9d2ea004707ca5
Mihai Pop <mihai.pop@blackline.com>
2019-11-29 14:58:25 +0200

    added vehicle class

---
+++ Proiect_tema_git/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    public class Vehicle
+    {
+        public string Make;
+
+        public string Model;
+
+        public string Model2;
+
+        public string Registration;
+
+        public string YearOfInitialRegistration;
+
+        public int CurrentValue1;
+    }
+}
