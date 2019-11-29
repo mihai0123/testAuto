62a6b38338bdb3dacee044322cdc723b4a315110
Mihai Pop <mihai.pop@blackline.com>
2019-11-29 12:50:08 +0200

    added problem one

--- Proiect_tema_git/Program.cs
+++ Proiect_tema_git/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
-namespace Proiect_tema_git
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+
+        List<string> strList = new List<string>();
+        strList.Add("anA");
+        strList.Add("elena");
+        strList.Add("MARIA");
+        strList.Add("cRisTiAn");
+
+        TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+        string lowerNames = "";
+        string concatenatedNames = "";
+
+        for (int i = 0; i < strList.Count; i++)
         {
-           
+            lowerNames = lowerNames + strList[i].ToLower() + ", ";
+            concatenatedNames = concatenatedNames + strList[i] + ", ";
         }
+
+        Console.WriteLine("{0} -> {1}", concatenatedNames.Remove(concatenatedNames.Length - 2),
+            myTI.ToTitleCase(lowerNames).Remove(lowerNames.Length - 2));
+
+        Console.ReadLine();
+
     }
 }
