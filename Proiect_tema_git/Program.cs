87984a926fb103790fdfdaa210f39dde328173da
Mihai Pop <mihai.pop@blackline.com>
2019-11-29 12:54:54 +0200

    added problem two

--- Proiect_tema_git/Program.cs
+++ Proiect_tema_git/Program.cs
@@ -27,7 +27,38 @@ public class Program
         Console.WriteLine("{0} -> {1}", concatenatedNames.Remove(concatenatedNames.Length - 2),
             myTI.ToTitleCase(lowerNames).Remove(lowerNames.Length - 2));
 
-        Console.ReadLine();
+        displayNamesVowels(strList);
+        displayNamesConsonants(strList);
 
+    }
+
+    public static void displayNamesVowels(List<string> elements)
+    {
+        string vowels = "aeiou";
+        foreach (var element in elements)
+        {
+            bool startsWithVowel = vowels.Contains((element[0]).ToString().ToLower());
+            if (startsWithVowel)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+        Console.ReadLine();
+    }
+
+    public static void displayNamesConsonants(List<string> elements)
+    {
+        string consonants = "bcdfghjklmnpqrstvwxz";
+        foreach (var element in elements)
+        {
+            bool startsWithConsonant = consonants.Contains((element[0]).ToString().ToLower());
+            if (startsWithConsonant)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+        Console.ReadLine();
     }
 }
